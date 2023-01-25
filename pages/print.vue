8f990ba324a0e38e12ca59bec0f12db1404a0e40
songrit0 <songrit208133@gmail.com>
2023-01-25 16:02:10 +0700

    25/1/2566

--- pages/print.vue
+++ pages/print.vue
@@ -1,28 +1,372 @@
 <template>
 	<div class="">
-		
+		<div class="page_pages">
+			<br>
+			<div class="row col-12">
+				<div class="col-4">งานเทคโนโลยีสารสนเทศ</div>
+				<div class="col-8 text-end">
+					<div class="form-check form-check-inline">
+						<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+							viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>
+						<label class="form-check-label" for="inlineCheckbox1">HOSxP</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+							viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>
+						<label class="form-check-label">Back Office</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+							viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>
+						<label class="form-check-label">เร่งด่วน</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+							viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>
+						<label class="form-check-label">ปกติ</label>
+					</div>
+				</div>
+			</div>
+			<div class="row col-12">
+				<p>เลขที่............................</p>
+			</div>
+			<div class="row col-12 justify-content-center">
+				<p class="col-7 text-center" style="font-size: 18px;">ใบส่งช่อมครุภัณฑคอมพิวเตอร์และอุปกรณ์ต่อพ่วง</p>
+			</div>
+			<div class="row col-12">
+				<li class="text-decoration">ส่วนที่ 1 สำหรับผู้แจ้งซ่อม</li>
+			</div>
+			<div class="row col-12">
+				<div class="col-3">วันที.</div>
+				<div class="col-4">เดือน.</div>
+				<div class="col-2">พ.ศ.</div>
+				<div class="col-3">เวลา. 00:00 น.</div>
+			</div>
+			<div class="row col-12">
+				<div class="col-4">กลุ่มงาน.</div>
+				<div class="col-4">งาน.</div>
+				<div class="col-4">โทร.</div>
+			</div>
+			<div class="row col-12">
+				<div class="col-6">ชื่อคอมพิวเตอร์.</div>
+				<div class="col-6">ชื่อผู้รับผิดชอบ.</div>
+			</div>
+			<div class="row col-12">
+				<div class="col-6">รหัสครุภัณฑ์.</div>
+				<div class="col-6">Ip Address.</div>
+			</div>
+			<div class="row col-12">
+				<li class="text-decoration">อุปกรณ์ในการส่งซ่อมคอมพิวเตอร์</li>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">คอมพิวเตอร์แม่ข่าย</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">คอมพิวเตอร์ตั้งโต๊ะ(pc)</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">คอมพิวเตอร์พกพา(NoteBook)</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">เครื่องสแกนเนอร์(Scnner)</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">ครื่องพิมพ์(printer)</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">แป้นพิมพ์(KeyBoard)</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">เม้าส์usb(mouseusb)</label>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="form-check form-check-inline">
+							<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+								viewBox="0 0 16 16">
+								<path
+									d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+							</svg>
+							<label class="form-check-label" for="inlineCheckbox1">อื่นๆ.......................</label>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row col-12">
+				<li class="">ปัญหา/อาการ</li>
+			</div>
+			<div class="row col-12">
+				<li class="">..............................</li>
+			</div>
+			<div class="row justify-content-end">
+				<div class="row col-6 justify-content-center">
+					<li class="col-6 ">ลงชื่อ</li>
+					<li class="col-6">ผู้ส่งซ่อม</li>
+				</div>
+			</div>
+			<div class="row justify-content-end">
+				<div class="row col-7 justify-content-center">
+					<li class="col-12 text-center">( ทรงฤทธิ์ ลุมไธสง )</li>
+				</div>
+			</div>
+			<div class="row col-12">
+				<li class="text-decoration">ส่วนที่ 2 สำหลับเจ้าหน้าที่ประสานงานซ่อม</li>
+			</div>
+			<div class="row col-12">
+				<div class="col-3">วันที.</div>
+				<div class="col-4">เดือน.</div>
+				<div class="col-2">พ.ศ.</div>
+				<div class="col-3">เวลา. 00:00 น.</div>
+			</div>
+			<div class="row col-12">
+				<li class="">ผู้ประสานงาน/ผู้รับผิดชอบ</li>
+			</div>
+			<div class="row justify-content-end">
+				<div class="row col-12 justify-content-center">
+					<div class="form-check form-check-inline" v-for="item in Coordinator  " :key="item">
+						<svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" class="bi bi-app"
+							viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>
+						<label class="form-check-label col-3 px-2"> {{ item.recipients }}</label>
+						<label class="form-check-label col-3"> {{ item.positions }}</label>
+						<label
+							class="form-check-label col-5">วันที่รับเรื่อง.............เดือน............พ.ศ.............</label>
+					</div>
+				</div>
+			</div>
+			<div class="row col-12">
+				<li class="text-decoration">ส่วนที่ 3 สำหลับเจ้าหน้าที่รับหมายและผู้ตรวจงาน</li>
+			</div>
+			<ul>
+				<div class="row col-12">
+					<div class="col-4"><svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor"
+							class="bi bi-app" viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>ส่งซ่อม</div>
+					<div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor"
+							class="bi bi-app" viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>ซ่อมได้</div>
+					<div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor"
+							class="bi bi-app" viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>เบิกอะไหล่</div>
+					<div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor"
+							class="bi bi-app" viewBox="0 0 16 16">
+							<path
+								d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+						</svg>ซ่อมไม่คุ้มค่า</div>
+				</div>
+			</ul>
+			<div class="row col-12">
+				<li class="">รายการสิ่งที่ต้องการเบิก/จัดซื้อ</li>
+			</div>
+			<div class="row col-12">
+				<table class="">
+					<tr>
+						<th style="width: 1cm;">ลำดับ</th>
+						<th style="width: 3cm;">รายการ</th>
+						<th style="width: 1cm;">จำนวน</th>
+						<th style="width: 1cm;">ราคาต่อหน่วย</th>
+						<th style="width: 1cm;">ราคารวม</th>
+						<th style="width: 2cm;">วันทีส่งเบิก</th>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+
+					</tr>
+
+				</table>
+
+
+			</div>
+		</div>
+
+		<!-- <div class="show-information">
+			
+			<div class="show_i-div2">
+				<div class="row col-12">
+					
+					<h2 class="col-12 py-1">ข้อมมูลการแจ้งซ่อม</h2>
+					<div class="col-6 py-2">
+						<li><b>ชื้อผู้แจ้งซ่อม: </b>{{ check(GETBYID?.name_sender) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>เบอร์โทรศัพท์: </b>{{ check(GETBYID?.phone) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>กลุ่มงาน: </b>{{ check(GETBYID?.work_group) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>งาน: </b>{{ check(GETBYID?.work) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>ต้องการแจ้งซ่อม: </b>{{ check(GETBYID?.equipment) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>ชื่อ คอมพิวเตอร์/อุปกรณ์: </b>{{ check(GETBYID?.computer_name) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>รหัสครุภัณฑ์: </b>{{ check(GETBYID?.commodity_code) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>ip_address: </b>{{ check(GETBYID?.ip_address) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>ปัญหา/อาการ: </b>{{ check(GETBYID?.problem_symptom) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>ความเร่งด่วน: </b>{{ check(GETBYID?.requirements) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>อื่นๆ: </b>{{ check(GETBYID?.other) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>วัน/เวลาที่แจ้ง:</b>{{ setday(GETBYID?.date_repair) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>วัน/เวลารับแจ้ง:</b> {{ setday(GETBYID?.date_receive) }}</li>
+					</div>
+					<div class="col-6 py-2">
+						<li><b>ผู้ประสานงาน(ผู้รับแจ้งซ่อม):</b> {{ check(GETBYID?.name_responsible) }}</li>
+					</div>
+				
+
+				</div>
+			</div>
+		</div> -->
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+import { URL_IP, URL_PUT_GET_BY_ID, URL_PUT_PROCESS } from '../constants'
+import Swal from 'sweetalert2'
 export default {
 	data() {
 		return {
-			login: false
+			ID: 2,
+			GETBYID: '',
+			Coordinator: ''
+
 		}
 	},
 	methods: {
-		button() {
-			this.login = !this.login
-			setTimeout(() => {
-				window.print()
-			}, 1000);
-			setTimeout(() => {
-				this.login = !this.login
-			}, 10000);
+		Showformitem(payload) {
+			$nuxt.$store.commit('setShowformitem', payload)
+		},
+		check(item) {
+			if (item) {
+				return item
+			} else {
+				return 'ยังไม่มีข้อมูล'
+			}
+		},
+		// ${this.$store.state.showformitem.id}
+		GET_by_id() {
+			axios.get(`${URL_PUT_GET_BY_ID}/1`).then(response => {
+				this.GETBYID = response.data.results
+			})
+		},
+		GET_coordinator() {
+			axios.get(`${URL_IP}/Getall_recipient`).then(response => {
+				this.Coordinator = response.data.results
+			})
+		},
+		setday(item) {
+			if (item) {
+				var H = new Date(item).getHours()
+				var Ms = new Date(item).getMinutes()
+				var D = new Date(item).getDate()
+				var M = new Date(item).getMonth() + 1
+				var Y = new Date(item).getFullYear()
+				return ` ${D} / ${M} / ${Y} เวลา ${H}:${Ms} น.`
+			} else {
+				return `ไม่มีข้อมูล`
+			}
 
 		}
+
+	},
+	mounted() {
+		this.GET_coordinator()
+		this.GET_by_id()
+		console.log(this.GETBYID);
+
 	}
+
 
 }
 </script>
@@ -33,7 +377,72 @@ export default {
 	margin: 0;
 }
 
-.pagenone {
-	/* display: none */
+.page_pages svg {
+	margin: 0px 0px 3px 0px;
+}
+
+.show_i-div2 {
+	display: flex;
+	height: auto;
+	/* width: 70%;  */
+	justify-content: center;
+	/* padding: 15px 30px 15px 30px; */
+	/* background-color: #d4d4d4; */
+	z-index: 6;
+	border-radius: 12px;
+
+}
+
+.form-check {
+	padding-left: 10px;
+}
+
+.show-information {
+	display: flex;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	/* max-height: 19cm; */
+	height: 777px;
+	padding: 150px;
+	justify-content: center;
+	z-index: 5;
+	/* filter: blur(4px); */
+}
+
+
+.back-button {
+	display: flex;
+	justify-content: flex-end;
+}
+
+.page_pages {
+	display: flex;
+	/* position: fixed; */
+	padding: 30px 55px 20px 55px;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: azure;
+	z-index: 10;
+	flex-direction: column;
+}
+
+.text-decoration {
+	text-decoration: underline;
+}
+
+table,
+td,
+th {
+	border-collapse: collapse;
+	border: 1px solid #000000;
+	text-align: center;
+}
+
+table td {
+	height: 45px;
 }
 </style>
