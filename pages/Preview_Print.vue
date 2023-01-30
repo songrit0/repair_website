20b27449858c267aefcced923e4521f6dc3468d9
songrit0 <songrit208133@gmail.com>
2023-01-30 11:39:15 +0700

    back button

--- pages/Preview_Print.vue
+++ pages/Preview_Print.vue
@@ -9,7 +9,12 @@
 					<path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
 					<path
 						d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
-				</svg> พิมพ์</button></div>
+				</svg> พิมพ์</button> <button  @click="$router.back(1)" type="button" class="btn btn-danger"><svg
+					xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#fff"
+					class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+					<path fill-rule="evenodd"
+						d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
+				</svg>กลับ</button></div>
 		<br>
 		<div class="PV_print-item">
 			<print :printON='printON' :printID="printID" />
